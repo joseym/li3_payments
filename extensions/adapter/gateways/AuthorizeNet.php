49051de9c5869e1c740064d34a26c01c24f7c241
Josey Morton <me@joseymorton.com>
2012-10-22 14:06:42 -0500

    more authorize api methods to create customer and payment profiles

--- extensions/adapter/gateways/AuthorizeNet.php
+++ extensions/adapter/gateways/AuthorizeNet.php
@@ -4,6 +4,7 @@ namespace li3_payments\extensions\adapter\gateways;
 use AuthorizeNetCIM;
 use AuthorizeNetCustomer;
 use AuthorizeNetPaymentProfile;
+use AuthorizeNetTransaction;
 
 class AuthorizeNet extends \lithium\core\Object {
 
@@ -13,13 +14,30 @@ class AuthorizeNet extends \lithium\core\Object {
 	protected $_customer;
 	protected $_payment;
 	protected $_customerProfile;
+	protected $_transaction;
+	protected $_mode; // "none", "testMode", "liveMode"
 
 	protected $_autoConfig = array('login', 'key');
 
+	/**
+	 * Test card numbers for AuthNet sandbox
+	 * @var array
+	 */
+	public static $cards = array(
+		'dinersclub' 	=> '38000000000006',
+		'amex'			=> '370000000000002',
+		'disc'			=> '6011000000000012',
+		'visa1'			=> '4007000000027',
+		'visa2'			=> '4012888818888',
+		'jcb'			=> '3088000000000017'
+	);
+
 	public function __construct(array $config = array()) {
+		$this->_mode = isset($config['validation_mode']) ? $config['validation_mode'] : 'none';
 		$this->_customer = new AuthorizeNetCustomer();
 		$this->_payment = new AuthorizeNetPaymentProfile();
 		$this->_customerProfile = new AuthorizeNetCIM($config['login'], $config['key']);
+		$this->_transaction = new AuthorizeNetTransaction;
 		return parent::__construct($config);
 	}
 
@@ -29,6 +47,10 @@ class AuthorizeNet extends \lithium\core\Object {
 
 	public function login(){
 		return $this->_login;
+	}
+
+	public function card($provider = 'visa1'){
+		return static::$cards[$provider];
 	}
 
 	/**
@@ -41,8 +63,8 @@ class AuthorizeNet extends \lithium\core\Object {
 
 		$defaults = array(
 			'customerType' => 'individual',
-			'cardNumber' => '4007000000027',
-			'expiration' => date('my', strtotime('next year'))
+			'cardNumber' => $this->card(),
+			'expiration' => date('Y-m', strtotime('next year'))
 		);
 
 		$payment += $defaults;
@@ -55,39 +77,63 @@ class AuthorizeNet extends \lithium\core\Object {
 
 	}
 
-	public function customer(array $customer = array()){
+	/**
+	 * Creates a customer profile
+	 * @param  array  $customer required options to pass to authnet
+	 * @return int           customer profile id
+	 */
+	public function customerProfile(array $customer = array()){
 
 		$defaults = array(
 			'description' => 'John Doe Payment Information',
 			'email' => 'test@dev.com',
 		);
 
+		$defaults['customerid'] = substr(sha1(
+			"{$defaults['description']}{$defaults['email']}"
+		), 0, 20);
+
 		$customer += $defaults;
 
 		$this->_customer->description = $customer['description'];
 		$this->_customer->email = $customer['email'];
-		$this->_customer->merchantCustomerId = substr(sha1(
-			"{$customer['description']}{$customer['email']}"
-		), 0, 20);
+		$this->_customer->merchantCustomerId = $customer['customerid'];
 
-		return $this->_customerProfile->createCustomerProfile($this->_customer);
+		$response = $this->_customerProfile->createCustomerProfile($this->_customer, $this->_mode);
+
+		$return = new \stdClass();
+
+		if($response->isOk()){
+
+			$return->status = 'success';
+			$return->profile_id = $response->getCustomerProfileId();
+			$return->payment_profile = $response->getCustomerPaymentProfileIds();
+
+		} else {
+			
+			$return->status = 'failure';
+			$return->code = $response->getMessageCode();
+			$return->message = $response->getMessageText();
+
+			// hacky way of returning duplicate profile id - may not even be useful
+			if($response->getMessageCode() == 'E00039' 
+				&& preg_match_all('/([0-9]+)/', $response->getMessageText(), $matches)){
+				$return->profile_id = $matches[0][0];
+			}
+
+		}
+
+		return $return;
 
 	}
 
-	public function getCustomerProfile() {
-
-		print_r($this->customer()->getCustomerProfileId()); exit;
-
-		// if (!$response->isOk()) {
-		// 	return false;
-		// }
-		// unset($entity->creditCard, $entity->expiration);
-		// $entity->profileId = $response->getCustomerProfileId();
-		// $entity->profileLookupId = $customer->merchantCustomerId;
-		// return true;
-		
+	public function customerPayment($payment){
+		if(get_class($payment) == 'AuthorizeNetPaymentProfile'){
+			return $this->_customer->paymentProfiles[] = $payment;
+		} else {
+			return false;
+		}
 	}
-
 
 }
 
